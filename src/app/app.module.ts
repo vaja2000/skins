85d93805794e26de1eb008ea25a504444430384c
vaja <74463035+vaja2000@users.noreply.github.com>
2023-06-21 14:39:06 +0400

    background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './view/navbar/navbar.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { MainComponent } from './view/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
